676481fa35d3d668bf1ec59a0292d7384f3aec1d
I-k31 <Izan.kh31@Gmail.com>
2025-12-26 01:14:50 -0500

    Update

---
+++ RCODE.R
@@ -0,0 +1,11 @@
+data(UCBAdmissions)
+ucb=as.data.frame(UCBAdmissions)
+head(ucb)
+
+ucb$Admit=relevel(ucb$Admit, ref = "Rejected")
+
+model1 = glm(Admit ~ Gender + Dept,
+              data = ucb,
+              weights = Freq,
+              family = binomial)
+exp(cbind(OR = coef(model1), confint.default(model1)))
